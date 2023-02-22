3d9861a4f2fed1ca93654dcf141f8aeb71171e6f
im8127 <ivy.mboya@accenture.com>
2023-02-22 10:55:54 +0000

    passing test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const divide = (num1, num2) => {
 };
 
 const subtract = (num1, num2) => {
-  return num2 - num1;
+  return num2 + num1;
 };
 
 const multiply = (num1, num2) => {
@@ -15,9 +15,10 @@ const multiply = (num1, num2) => {
 };
 
 const exponent = (num1, num2) => {
-  return num1 + num2;
+  return num1 ** num2;
 };
 
+//
 module.exports = {
   add,
   divide,
